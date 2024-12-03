8794218cc63f33fca45a4f1a7f75108e778074b1
pplancq <paul.plancq@outlook.fr>
2024-12-03 19:32:34 +0100

    Initial commit

---
+++ src/bootstrap.tsx
@@ -0,0 +1,26 @@
+import { App } from '@Front/components/App/App';
+import { StrictMode } from 'react';
+import { createRoot, type Root } from 'react-dom/client';
+
+// eslint-disable-next-line import/no-default-export
+export default class Bootstrap extends HTMLElement {
+  private readonly root: Root;
+
+  constructor() {
+    super();
+
+    this.root = createRoot(this);
+  }
+
+  connectedCallback() {
+    this.root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    );
+  }
+
+  disconnectedCallback() {
+    queueMicrotask(() => this.root.unmount());
+  }
+}

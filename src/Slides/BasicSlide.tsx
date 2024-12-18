57e69d9e92010b0fedacb3beca58808d5814e895
pplancq <paul.plancq@outlook.fr>
2024-12-18 10:56:45 +0100

    feat: add Note

--- src/Slides/BasicSlide.tsx
+++ src/Slides/BasicSlide.tsx
@@ -1,3 +1,4 @@
+import { Note } from '@Front/components/Note';
 import { Paragraph } from '@Front/components/Paragraph';
 import { Slide } from '@Front/components/Slide';
 import { Title } from '@Front/components/Title';
@@ -6,7 +7,11 @@ export const BasicSlide = () => {
   return (
     <Slide>
       <Title variant="h2">Basic Slide</Title>
-      <Paragraph>This is a basic slide with simple text.</Paragraph>
+      <Paragraph>This slide use reveal.js framework</Paragraph>
+      <Paragraph>
+        Press "f" to enter full screen mode, and press "s" to enter speaker mode. Use arrow keys to navigate between
+      </Paragraph>
+      <Note>This is a note. You can use it to provide additional information to the audience.</Note>
     </Slide>
   );
 };

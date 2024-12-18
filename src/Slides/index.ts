53f0f7144da01f27a6a75e917ad4c1825273f15f
pplancq <paul.plancq@outlook.fr>
2024-12-18 10:47:07 +0100

    feat: add MarkdownBlock and CodeBlock

--- src/Slides/index.ts
+++ src/Slides/index.ts
@@ -1,7 +1,8 @@
 import { BasicSlide } from '@Front/Slides/BasicSlide';
 import { FragmentSlide } from '@Front/Slides/FragmentSlide';
+import { MarkdownSlide } from '@Front/Slides/MarkdownSlide';
 import { TitleSlide } from '@Front/Slides/TitleSlide';
 import { VerticalSlide } from '@Front/Slides/VerticalSlide';
 import { type ComponentType } from 'react';
 
-export const slides = [TitleSlide, BasicSlide, VerticalSlide, FragmentSlide] as ComponentType[];
+export const slides = [TitleSlide, BasicSlide, VerticalSlide, FragmentSlide, MarkdownSlide] as ComponentType[];
